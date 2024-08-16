7999301bdb95701683e6c3a02c7ae12c35a394f0
Rudy Hentzen <rudy.hentzen@ft.com>
2024-08-16 15:48:17 +0100

    chore: import order and stylistic

--- index.js
+++ index.js
@@ -1,34 +1,50 @@
-import js from '@eslint/js';
-import prettier from 'eslint-plugin-prettier';
+import js from "@eslint/js";
+import prettier from "eslint-plugin-prettier";
 
 export default [
-	js.configs.recommended,
-	{
-		languageOptions: {
-			ecmaVersion: 'latest',
-			sourceType: 'module',
-			globals: {
-				vi: 'readonly',
-				describe: 'readonly',
-				it: 'readonly',
-				expect: 'readonly',
-				beforeEach: 'readonly',
-				afterEach: 'readonly',
-				fetch: 'readonly',
-				FileReader: 'readonly',
-				document: 'readonly',
-				localStorage: 'readonly',
-				process: 'readonly',
-				Buffer: 'readonly',
-			},
-		},
-		plugins: {
-			prettier: prettier,
-		},
-		rules: {
-			'no-console': 'error',
-			'no-unused-vars': 'error',
-			'prettier/prettier': 'error',
-		},
-	},
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        vi: "readonly",
+        describe: "readonly",
+        it: "readonly",
+        expect: "readonly",
+        beforeEach: "readonly",
+        afterEach: "readonly",
+        fetch: "readonly",
+        FileReader: "readonly",
+        document: "readonly",
+        localStorage: "readonly",
+        process: "readonly",
+        Buffer: "readonly",
+      },
+    },
+    plugins: {
+      prettier: prettier,
+    },
+    rules: {
+      "comma-dangle": "off",
+      "import/order": [
+        "error",
+        {
+          groups: ["builtin", "external", "internal"],
+          "newlines-between": "always",
+          alphabetize: {
+            order: "asc",
+            caseInsensitive: false,
+          },
+        },
+      ],
+      "no-console": "error",
+      "no-unused-vars": "error",
+      "prettier/prettier": "error",
+      quotes: "off",
+      semi: "off",
+      "semi-style": "off",
+      "space-before-function-paren": "off",
+    },
+  },
 ];
